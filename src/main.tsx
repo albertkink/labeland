22fbea6240941758e1ad2c45a0933c41bec33146
albertkink <david.schicho@proton.me>
2026-01-25 05:34:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,13 @@ import "flatpickr/dist/flatpickr.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
-import { CartProvider } from "./context/CartContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <CartProvider>
-        <AppWrapper>
-          <App />
-        </AppWrapper>
-      </CartProvider>
+      <AppWrapper>
+        <App />
+      </AppWrapper>
     </ThemeProvider>
   </StrictMode>,
 );
